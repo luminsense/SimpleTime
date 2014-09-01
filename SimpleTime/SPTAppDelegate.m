aaadf3f6c9a0f08f35fa4aa8c027f215fcf2b507
luminsense <wulianglumin@foxmail.com>
2014-09-01 19:04:12 +0800

    minor changes

--- SimpleTime/SPTAppDelegate.m
+++ SimpleTime/SPTAppDelegate.m
@@ -16,10 +16,7 @@
     [SPTEventStore sharedStore];
     [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
     
-    UILocalNotification *notification = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
-    if (notification) {
-        application.applicationIconBadgeNumber = 0;
-    }
+    application.applicationIconBadgeNumber = 0;
     
     return YES;
 }
@@ -53,6 +50,8 @@
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     // NSLog(@"Application did become activeo");
+    
+    application.applicationIconBadgeNumber = 0;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
